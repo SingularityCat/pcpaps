fa172667fffa58d4f87d625546a2553b184355ad
Elliot Thomas <e.singularitycat@gmail.com>
2015-02-02 19:37:33 +0000

    Fix structure definitions, implement more of 'read_packet'

--- src/pcap.py
+++ src/pcap.py
@@ -1,4 +1,3 @@
-
 import struct
 
 # libpcap's global header
@@ -30,44 +29,93 @@ PCAP_LE_NANOSEC = b"\x4d\x3c\xb2\xa1"
 PCAP_BE_REGULAR = b"\xa1\xb2\xc3\xd4"
 PCAP_BE_NANOSEC = b"\xa1\xb2\x3c\x4d"
 
-# Struct format strings for the above C structures in little/big endian forms.
-# Note that the 'magic_number' field is omitted from the *_GLOB_HDR strings.
-PCAP_LE_GLOB_HDR = "<H<H<i<I<I<I"
-PCAP_LE_PKT_HDR = "<I<I<I<I"
-PCAP_BE_GLOB_HDR = ">H>H>i>I>I>I"
-PCAP_BE_PKT_HDR = ">I>I>I>I"
+# Struct objects for the above C structures in little/big endian forms.
+# Note that the 'magic_number' field is omitted from the *_GLOB_HDR definitons.
+PCAP_LE_GLOB_HDR = struct.Struct("<HHiIII")
+PCAP_LE_PKT_HDR = struct.Struct("<IIII")
+PCAP_BE_GLOB_HDR = struct.Struct(">HHiIII")
+PCAP_BE_PKT_HDR = struct.Struct(">IIII")
+
+# Major/minor versions of the pcap file format.
+PCAP_MAJOR_VER = 2
+PCAP_MINOR_VER = 4
+
+class PcapFormatError(Exception):
+    """Exception raised when a file format error occurs."""
+    pass
 
 class PcapReader:
+    """PcapReader: reader for pcap files."""
     def __init__(self, fstream, magic=None):
         """Creates a PcapReader from an open stream.
 Takes one mandatory and one optional argument,
  - fstream: The readable stream to use.
- - magic: The first four bytes of the file. If this is specified, """
+ - magic: The first four bytes of the file. If this is None, four bytes are read from the stream first."""
 
         self.stream = fstream
 
         # Magic number: [a1 b2 c3 d4] OR [a1 b2 3c 4d] if file has nanosecond resolution.
         # This is stored in the same endianess as the rest of the integer data in this file,
         # giving four possible values this could be.
-        if magic = None:
+        if magic == None:
             magic = self.stream.read(4)
 
-        # Store the start of the file, sans magic number.
+        # Determine 
+        if magic.startswith(b"\xa1\xb2"):
+            # File is big-endian...
+            global_header_struct = PCAP_BE_GLOB_HDR
+            self.packet_header_struct = PCAP_BE_PKT_HDR
+
+            if magic.endswith(b"\xc3\xd4"):
+                # Regular (microsecond) big-endian pcap file
+                pass
+            elif magic.endswith(b"\x3c\x4d"):
+                # Nanosecond big-endian pcap file
+                pass
+            else:
+                raise PcapFormatError("Invalid magic {0}".format(magic))
+
+        elif magic.endswith(b"\xb2\xa1"):
+            # File is little-endian...
+            global_header_struct = PCAP_LE_GLOB_HDR
+            self.packet_header_struct = PCAP_LE_PKT_HDR
+
+            if magic.startswith(b"\xd4\xc3"):
+                # Regular (microsecond) little-endian pcap file
+                pass
+            elif magic.startswith(b"\xd4\xc3"):
+                # Nanosecond little-endian pcap file
+                pass
+            else:
+                raise PcapFormatError("Invalid magic {0}".format(magic))
+        else:
+            raise PcapFormatError("Invalid magic {0}".format(magic))
+    
+        # Read header
+        global_header_data = self.stream.read(global_header_struct.size)
+        self.version_major, self.version_minor, self.thiszone, self.sigfigs, self.snaplen, self.network = global_header_struct.unpack(global_header_data)
+
+        # Store the start of the file, sans header.
         self.startpos = self.stream.tell()
 
-        if magic.startswith(b"\xa1")
+
+    def read_packet(self, data=True):
+        """Reads a packet record header, and optionally, it's data, from the stream."""
+        packet_header_data = self.stream.read(self.packet_header_struct.size)
+        # Test for EOF or truncation
+        if packet_header_data == b"":
+            #EOF
+            return None
+        elif 0 < len(packet_header_data) < self.packet_header_struct.size:
+            # Truncation
+            raise PcapFormatError("Truncated file")
             
-    
-def pcap_read_packet(self, data=True):
-    """Reads a packet record header, and optionally, it's data, from a file stream."""
-    head = pcap_read_packet_header(stream)
-    if data:
-        body = stream.read(head[2])
-    else
-        body = None
-        stream.seek(head[2], 1)
-    return head, body
+        
+        packet_header = self.packet_header_struct.unpack(packet_header_data)
 
-def pcap_read_packet_header(self):
-    """Reads a packet record header, returning a 4-tuple consisting of the """
-
+        if data:
+            packet_body = self.stream.read(packet_header[2])
+        else:
+            packet_body = None
+            self.stream.seek(head[2], 1)
+        return packet_header, packet_body
