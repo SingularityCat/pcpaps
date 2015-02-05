7d17dece9faf76ae4779862f2e657567d05a96e3
Elliot Thomas <e.singularitycat@gmail.com>
2015-02-05 11:21:10 +0000

    Refactored code, added PcapWriter

--- src/pcap.py
+++ src/pcap.py
@@ -1,4 +1,9 @@
 import struct
+from . import packetfile
+
+"""
+pcap: Contains classes for reading and writing to pcap files.
+"""
 
 # libpcap's global header
 #
@@ -40,82 +45,134 @@ PCAP_BE_PKT_HDR = struct.Struct(">IIII")
 PCAP_MAJOR_VER = 2
 PCAP_MINOR_VER = 4
 
-class PcapFormatError(Exception):
+
+# Magic number: [a1 b2 c3 d4] OR [a1 b2 3c 4d] if file has nanosecond
+# resolution. This is stored in the same endianess as the rest of the
+# integer data in this file, giving four possible values this could be.
+def pcap_magic_resolve(magic):
+    """Function that resolves pcap's magic number into an appropriate struct/scale.
+Raises PcapFormatError on invalid magic."""
+    if magic.startswith(b"\xa1\xb2"):
+        # File is big-endian...
+        global_header_struct = PCAP_BE_GLOB_HDR
+        packet_header_struct = PCAP_BE_PKT_HDR
+        if magic.endswith(b"\xc3\xd4"):
+            # Regular (microsecond) big-endian pcap file
+            timescale = 10**3
+        elif magic.endswith(b"\x3c\x4d"):
+            # Nanosecond big-endian pcap file
+            timescale = 10**6
+        else:
+            raise PcapFormatError("Invalid magic {0}".format(magic))
+    elif magic.endswith(b"\xb2\xa1"):
+        # File is little-endian...
+        global_header_struct = PCAP_LE_GLOB_HDR
+        packet_header_struct = PCAP_LE_PKT_HDR
+
+        if magic.startswith(b"\xd4\xc3"):
+            # Regular (microsecond) little-endian pcap file
+            timescale = 10**3
+        elif magic.startswith(b"\xd4\xc3"):
+            # Nanosecond little-endian pcap file
+            timescale = 10**6
+        else:
+            raise PcapFormatError("Invalid magic {0}".format(magic))
+    else:
+        raise PcapFormatError("Invalid magic {0}".format(magic))
+
+    return global_header_struct, packet_header_struct, timescale
+
+
+class PcapFormatError(RuntimeError):
     """Exception raised when a file format error occurs."""
     pass
 
-class PcapReader:
+
+class PcapRangeError(RuntimeError):
+    """Exception raised when a unrepresentable value is encountered."""
+    pass
+
+
+class PcapReader(packetfile.PacketReader):
     """PcapReader: reader for pcap files."""
     def __init__(self, fstream, magic=None):
         """Creates a PcapReader from an open stream.
 Takes one mandatory and one optional argument,
  - fstream: The readable stream to use.
- - magic: The first four bytes of the file. If this is None, four bytes are read from the stream first."""
-
+ - magic: The first four bytes of the file.
+   If this is None, four bytes are read from the stream first."""
         self.stream = fstream
 
-        # Magic number: [a1 b2 c3 d4] OR [a1 b2 3c 4d] if file has nanosecond resolution.
-        # This is stored in the same endianess as the rest of the integer data in this file,
-        # giving four possible values this could be.
-        if magic == None:
+        # Read magic number if not done so already.
+        if magic is None:
             magic = self.stream.read(4)
 
-        # Determine 
-        if magic.startswith(b"\xa1\xb2"):
-            # File is big-endian...
-            global_header_struct = PCAP_BE_GLOB_HDR
-            self.packet_header_struct = PCAP_BE_PKT_HDR
+        # Remember magic.
+        self.magic = magic
 
-            if magic.endswith(b"\xc3\xd4"):
-                # Regular (microsecond) big-endian pcap file
-                pass
-            elif magic.endswith(b"\x3c\x4d"):
-                # Nanosecond big-endian pcap file
-                pass
-            else:
-                raise PcapFormatError("Invalid magic {0}".format(magic))
-
-        elif magic.endswith(b"\xb2\xa1"):
-            # File is little-endian...
-            global_header_struct = PCAP_LE_GLOB_HDR
-            self.packet_header_struct = PCAP_LE_PKT_HDR
-
-            if magic.startswith(b"\xd4\xc3"):
-                # Regular (microsecond) little-endian pcap file
-                pass
-            elif magic.startswith(b"\xd4\xc3"):
-                # Nanosecond little-endian pcap file
-                pass
-            else:
-                raise PcapFormatError("Invalid magic {0}".format(magic))
-        else:
-            raise PcapFormatError("Invalid magic {0}".format(magic))
-    
+        # Understand magic. This throws a PcapFormatError if incorrect.
+        global_header_struct, self.packet_header_struct, self.timescale\
+            = pcap_magic_resolve(self.magic)
         # Read header
         global_header_data = self.stream.read(global_header_struct.size)
-        self.version_major, self.version_minor, self.thiszone, self.sigfigs, self.snaplen, self.network = global_header_struct.unpack(global_header_data)
 
-        # Store the start of the file, sans header.
-        self.startpos = self.stream.tell()
+        # Store header fields into attributes.
+        self.version_major, self.version_minor,\
+            self.thiszone,\
+            self.sigfigs,\
+            self.snaplen,\
+            self.network = global_header_struct.unpack(global_header_data)
 
-
-    def read_packet(self, data=True):
-        """Reads a packet record header, and optionally, it's data, from the stream."""
+    def read_packet(self):
+        """Reads a packet record header and it's data from the stream.
+Returns a PacketInfo tuple and data, or (None, None) on EOF"""
         packet_header_data = self.stream.read(self.packet_header_struct.size)
         # Test for EOF or truncation
         if packet_header_data == b"":
-            #EOF
-            return None
+            # EOF
+            return None, None
         elif 0 < len(packet_header_data) < self.packet_header_struct.size:
             # Truncation
-            raise PcapFormatError("Truncated file")
-            
-        
-        packet_header = self.packet_header_struct.unpack(packet_header_data)
+            raise PcapFormatError("Stream truncated.")
 
-        if data:
-            packet_body = self.stream.read(packet_header[2])
-        else:
-            packet_body = None
-            self.stream.seek(head[2], 1)
-        return packet_header, packet_body
+        packet_header = self.packet_header_struct.unpack(packet_header_data)
+        packet_data = self.stream.read(packet_header[2])
+
+        packet_info = packetfile.PacketInfo(self.network,
+            packet_header[0], packet_header[1] * (10**6 // self.timescale),
+            packet_header[2], packet_header[3])
+
+        return packet_info, packet_data
+
+
+class PcapWriter(packetfile.PacketWriter):
+    """"""
+    def __init__(self, stream, magic=PCAP_LE_REGULAR, snaplen=65535, network=1):
+        """Setup a new PcapWriter object, and write the header to file."""
+        self.stream = stream
+        self.snaplen = snaplen
+        self.network = network
+
+        # Understand magic. This throws a PcapFormatError if incorrect.
+        global_header_struct, self.packet_header_struct, self.timescale\
+            = pcap_magic_resolve(magic)
+
+        self.stream.write(magic)
+        self.stream.write(global_header_struct.pack(
+            PCAP_MAJOR_VER, PCAP_MINOR_VER, 0, 0,
+            self.snaplen, self.network))
+
+    def write_packet(self, packet_info, packet_data):
+        """Writes a packet record header and data to the stream."""
+        # Can only write one kind of network in a pcap file.
+        if packet_info.network != self.network:
+            raise PcapRangeError("Tried writing packet with a different network type")
+
+        # We truncate packet_data to either the packet_info or snaplen limits, if needed.
+        maxlen = min(self.snaplen, packet_info.caplen, len(packet_data))
+        packet_header_data = self.packet_header_struct.pack(
+            packet_info.timestamp, (packet_info.nanosec * self.timescale) // 10**6,
+            maxlen, packet_info.origlen)
+
+        self.stream.write(packet_header_data)
+        self.stream.write(packet_data[:maxlen])
