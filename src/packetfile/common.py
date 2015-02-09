9d69d4d860c77dab4bbb971966f4b855b09b48b0
Elliot Thomas <e.singularitycat@gmail.com>
2015-02-09 02:14:26 +0000

    Expanded 'Packet' namedtuple to contain comparison methods and the length method.

--- src/packetfile/common.py
+++ src/packetfile/common.py
@@ -9,12 +9,40 @@ common:
  - Abstract classes for packet capture file readers/writers.
 """
 
-Packet = collections.namedtuple("Packet", ["unixtime", "origlen", "data"])
-Packet.__doc___ = """\
-Named tuple consisting of three fields,
+class Packet(collections.namedtuple("Packet", ["unixtime", "origlen", "data"])):
+    """Named tuple consisting of three fields,
  - unixtime: Floating point number, time in seconds since 1st Jan, 1970.
  - origlen: Original length of the 'data' field.
- - data: Packet data."""
+ - data: Packet data.
+
+Comparison operator methods and the length method are implemented.
+Comparisons work on the value of 'unixtime', so a < b means a is older then b.
+The length is 'origlen', so len(a) < len(b) means a was shorter then b."""
+
+    # Implement comprison operations based on 'unixtime', such that:
+    # a < b means a is older then b.
+    def __lt__(self, other):
+        return self.unixtime < other.unixtime
+
+    def __le__(self, other):
+        return self.unixtime <= other.unixtime
+
+    def __eq__(self, other):
+        return self.unixtime == other.unixtime
+
+    def __ne__(self, other):
+        return self.unixtime != other.unixtime
+
+    def __gt__(self, other):
+        return self.unixtime > other.unixtime
+
+    def __ge__(self, other):
+        return self.unixtime >= other.unixtime
+
+    # Implement length func based on 'origlen', such that:
+    # len(a) < len(b) means a has an original length shorter then b.
+    def __len__(self):
+        return self.origlen
 
 
 def print_packetinfo(packet):
