34a73af150de67754e8b18992a79f4d108907327
Elliot Thomas <e.singularitycat@gmail.com>
2015-02-09 01:24:01 +0000

    Fix wrong exception class name in PacketFileUnknownFormatError. Fix incomplete class path for the packetfile_open_read function. Add packetfile_open_write function.

--- src/packetfile/packetfile.py
+++ src/packetfile/packetfile.py
@@ -4,12 +4,12 @@ from . import pcap
 
 #extension = os.path.splitext(fpath)[1]
 
-class PacketFileUnknwonFormatError(RuntimeException):
+class PacketFileUnknwonFormatError(RuntimeError):
     """Exception raised when a file is unable to be opened."""
     pass
 
-def packetfile_open_read(fpath):
-    """Function that tries to open a packet capture file."""
+def packetfile_open_read(fpath, **kwargs):
+    """Function that tries to open a packet capture file for reading."""
     extension = os.path.splitext(fpath)[1]
 
     # Open the file.
@@ -18,14 +18,26 @@ def packetfile_open_read(fpath):
     if extension == "pcap":
         # Try PcapReader if file extension is 'pcap':
         try:
-            return PcapReader(pfile)
-        except PcapFormatError err:
+            return pcap.PcapReader(pfile, **kwargs)
+        except pcap.PcapFormatError:
             pfile.seek(0, 0)
 
     # Dumb method
     try:
-        return PcapReader(pfile)
-    except PcapFormatError err:
+        return pcap.PcapReader(pfile, **kwargs)
+    except pcap.PcapFormatError:
         pfile.seek(0, 0)
 
-    
+    raise PacketFileUnknownFormatError("Can't identify file format.")
+
+
+def packetfile_open_write(fpath, **kwargs):
+    """Function that tries to open a packet capture file for writing."""
+    extension = os.path.splitext(fpath)[1]
+
+    # Open the file.
+    pfile = open(fpath, "wb")
+
+    if extension == "pcap":
+        # Use PcapWriter
+        return pcap.PcapWriter(pfile, **kwargs)
