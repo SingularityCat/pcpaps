6e50adf8477a5a2ead7c00ac247e473b99192a20
Elliot Thomas <e.singularitycat@gmail.com>
2015-03-28 22:06:24 +0000

    Expand on filter pipeline:  - Added function that compares two identity lists  - Implemented a proper filter

--- src/packet/pipeline/filter.py
+++ src/packet/pipeline/filter.py
@@ -1,11 +1,36 @@
+PERMIT=True
+DISCARD=False
 
-# 
-#
-def filter(source, deny=None, permit=None):
+def identity_match(packet_ident, proto_ident):
+    """Compares two identity lists to see if they match.
+Every protocol identity in proto_ident must match those
+in packet_ident sufficiently."""
+    match = False
+    for i in range(0, len(proto_ident)):
+        if packet_ident[i] != proto_ident[i]
+            break
+    else:
+        match = True
+    return match
+
+
+# Any identity matching something in the permit set is permitted.
+# Any identity matching something in the discard set is discarded.
+# Otherwise, the policy boolean is used.
+def filter(source, permit=None, discard=None, policy=PERMIT):
     if permit is None:
         permit = set()
     if deny is None:
         deny = set()
 
     for packet in source:
-        yield packet
+        permit = policy
+        for proto_ident in permit:
+            if identity_match(packet.identity, ident):
+                permit = True
+        else:
+            for proto_ident in discard:
+                if identity_match(packet.identity, proto_ident):
+                    permit = False
+        if permit:
+            yield packet
