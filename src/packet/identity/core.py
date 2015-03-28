e270e14657177f4f0fe61b7e9d5a6eed2f956b21
Elliot Thomas <e.singularitycat@gmail.com>
2015-03-28 22:00:42 +0000

    Added some docstrings to identity.core, Changed __eq__ to match, made __eq__ and __ne__ use said match method. Added match_protocol method.

--- src/packet/identity/core.py
+++ src/packet/identity/core.py
@@ -1,19 +1,41 @@
 """
 core: root module of identification system
+Contains class definition for ProtocolIdentity.
 """
 
 ATTRIBUTE_WILDCARD = None
 
 class ProtocolIdentity:
+    """A 'protocol identity'.
+This class represents a piece of data conforming to a protocol.
+An example of a protocol identitiy would be a IP header.
+It stores these identities in terms of 'attributes' in a dictionary.
+An attribute is a smaller piece of variable data in a protocol, for
+instance, an sender's IP address is a attribute of an IP header.
+It also stores a field called 'offset', which is how far into the
+packet the header of this protocol is. This field does not make sense
+for all protocols, or prototypes."""
     __slots__ = {"name", "attributes", "offset"}
     def __init__(self, name, attrs, offset):
+        """Constructor for 'protocol identities'. """
         self.name = name
         self.attributes = attrs
         self.offset = offset
 
-    def __eq__(self, target):
+    def match_protocol(self, target):
+        """Returns true if two protocol identities share the
+same protocol identifier."""
+        assert isinstance(target, ProtocolIdentity)
+        return self.name == target.name
+
+    def match(self, target):
+        """Tests wether this ProtocolIdentity matches another.
+This is not commutative - all of the target keys MUST be in
+this ProtocolIdentity's keys. Additionally, if the target's key is
+equal to None, it is treated as a wildcard and matches."""
         assert isinstance(target, ProtocolIdentity)
         if target.name == self.name:
+            # More convienient names.
             tattrs = target.attributes
             sattrs = self.attributes
             
@@ -21,14 +43,18 @@ class ProtocolIdentity:
             skeys = set(sattrs.keys())
             if tkeys <= skeys:
                 for key in tkeys & skeys:
-                    if sattrs[key] != tattrs[key] and sattrs[key] is not None:
+                    if sattrs[key] != tattrs[key] and \
+                        tattrs[key] is not ATTRIBUTE_WILDCARD:
                         break
                 else:
                     return True
-            
-            
         return False
 
+    def __eq__(self, target):
+        return self.match(target)
+
+    def __ne__(self, target):
+        return not self.match(target)
 
 def root_identify(packet, linktype):
     if linktype in registry:
