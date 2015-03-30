33dcfb97555580cd0a0b02ba5a45f9346b1dd97f
Elliot Thomas <e.singularitycat@gmail.com>
2015-03-30 22:13:06 +0000

    Expanded on ARP class.  - Implemented get_attributes  - Implemented build_attributes

--- src/packet/identity/arp.py
+++ src/packet/identity/arp.py
@@ -1,7 +1,9 @@
-import struct
+from .. import common
 
 from . import core
 from . import eth
+
+from .core import uint16pack, uint16unpack
 
 # RFC 826:
 #
@@ -23,13 +25,16 @@ from . import eth
 #        mbytes: (ar$tpa) Protocol address of target.
 
 
+# ARP hardware address types.
 ARP_HARDWARE_ETHERNET = 1
 
-# These match ethertypes.
+# ARP protocol address types. (these match ethertypes)
 ARP_PROTOCOL_IPV4 = 0x0800
 
+# ARP Opcodes.
 ARP_OPCODE_REQUEST = 1
 ARP_OPCODE_REPLY = 2
+
 
 class ARP(core.Protocol):
     name = "arp"
@@ -39,7 +44,7 @@ class ARP(core.Protocol):
 
     def __init__(self, packet, offset):
         super().__init__(packet, offset)
-        _calculate_offsets()
+        self._calculate_offsets()
 
     def _calculate_offsets(self):
         # Fixed offsets.
@@ -55,24 +60,80 @@ class ARP(core.Protocol):
 
         varoff = self.offset+8
 
-        _sha = slice(varoff, varoff+hlen)
-        _spa = slice(varoff+hlen, varoff+hlen+plen)
-        _tpa = slice(varoff+hlen+plen, varoff+2*hlen+plen)
-        _tha = slice(varoff+2*hlen+plen, varoff+2*hlen+2*plen)
+        self._sha = slice(varoff, varoff+hlen)
+        self._spa = slice(varoff+hlen, varoff+hlen+plen)
+        self._tpa = slice(varoff+hlen+plen, varoff+2*hlen+plen)
+        self._tha = slice(varoff+2*hlen+plen, varoff+2*hlen+2*plen)
 
     def get_attributes(self):
         """Retrieve a set of attributes describing fields in this protocol."""
-        return {}
+        return {
+            "htype": uint16unpack(self.packet.data[self._htype]),
+            "ptype": uint16unpack(self.packet.data[self._ptype]),
+            "hlen": self.packet.data[self._hlen],
+            "plen": self.packet.data[self._plen],
+            "opcode": uint16unpack(self.packet.data[self._opcode]),
+            "sha": self.packet.data[self._sha],
+            "spa": self.packet.data[self._spa],
+            "tha": self.packet.data[self._tha],
+            "tpa": self.packet.data[self._tpa]
+        }
 
     def set_attributes(self, attrs):
         """Alter packet data to match a set of protocol attributes."""
         pass
 
+    # ARP prototype attribute string format:
+    # <attr> = <key> "=" <value>
+    # <attrstr> = <attr> | <attr> ";" <attrstr>
+    #
+    # Attributes are seperated by semicolons, which contain colon-seperated key-value pairs.
+    # Valid keys are "sha", "spa", "tha", "tpa" or "opcode"
     @staticmethod
     def build_attributes(attrstr):
-        """Build attribute dict from string."""
-        attrs = {}
-        return attrs
+        """Creates a set of ARP attributes from an attribute string."""
+        attrdict = {}
+
+        attrs = attrstr.split(";")
+        for attr in attrs:
+            try:
+                # Split into kvp.
+                k, v = attr.split("=")
+
+                # Interpret integer fields. 
+                if k == "htype" or k == "ptype" or k == "opcode":
+                    i = common.parse_int(v)
+                    # Check if integer is valid.
+                    if i is not None:
+                        attrdict[k] = i
+
+                # Interpret (possible) MAC address fields.
+                elif k == "sha" or k == "tha":
+                    # First, try interpreting as a MAC address.
+                    ha = common.mac_str2bin(v)
+                    # Check if it's valid, if not, interpret as written hex encoded bytes.
+                    if ha is None:
+                        ha = common.parse_hexbytes(v)
+
+                    if ha is not None:
+                        attrdict[k] = ha
+
+                # Interpret (possible) IPv4 address fields.
+                elif k == "spa" or k == "tpa":
+                    # First, try interpreting as an IPv4 address.
+                    pa = common.ip4_str2bin(v)
+                    # Check if it's valid, if not, interpret as written hex encoded bytes.
+                    if pa is None:
+                        pa = common.parse_hexbytes(v)
+
+                    if pa is not None:
+                        attrdict[k] = pa
+
+            except ValueError:
+                # Skip malformed attribute.
+                pass
+
+        return attrdict
 
     @staticmethod
     def interpret_packet(packet, offset):
