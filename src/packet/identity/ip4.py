c791d90cc0b7658ca6a01c9ee3fd2575ed23d767
Elliot Thomas <e.singularitycat@gmail.com>
2015-03-30 14:06:59 +0100

    Rewrote ip4.py to use new class based interface.

--- src/packet/identity/ip4.py
+++ src/packet/identity/ip4.py
@@ -1,27 +1,33 @@
-import sys
 import struct
 
 from . import core
 from . import eth
 
-identifier = "ip4"
 
-def identify(packet, offset):
-    attrs = {}
-    packet.identity.append(core.ProtocolIdentity(identifier, attrs, offset))
+class IPv4(core.Protocol):
+    name = "ip4"
 
+    def __init__(self, packet, offset):
+        super().__init__(packet, offset)
 
-def prototype(attrstr):
-    pass
+    def get_attributes(self):
+        """Retrieve a set of attributes describing fields in this protocol."""
+        return {}
 
+    def set_attributes(self, attrs):
+        """Alter packet data to match a set of protocol attributes."""
+        pass
 
-def modify(packet, ididx, prototype):
-    pass
+    @staticmethod
+    def build_attributes(attrstr):
+        """Build attribute dict from string."""
+        attrs = {}
+        return attrs
 
+    @staticmethod
+    def interpret_packet(packet, offset):
+        """Interpret packet data for this protocol."""
+        instance = IPv4(packet, offset)
 
-def register(ip4proto, module):
-    registry[ip4proto] = module
-
-registry = {}
-
-eth.register(eth.ETHERTYPE_IP4, sys.modules[__name__])
+core.register_protocol(IPv4)
+eth.register_ethertype(IPv4.name, eth.ETHERTYPE_IP4)
