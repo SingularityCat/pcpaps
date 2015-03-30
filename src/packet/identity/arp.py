f8c948f93badcf6a8f393cad0e961a28aff061c4
Elliot Thomas <e.singularitycat@gmail.com>
2015-03-30 13:59:17 +0100

    Rewrote arp.py to use new class based interface.

--- src/packet/identity/arp.py
+++ src/packet/identity/arp.py
@@ -1,24 +1,33 @@
-import sys
 import struct
 
 from . import core
 from . import eth
 
-identifier = "arp"
 
+class ARP(core.Protocol):
+    name = "arp"
 
-def identify(packet, offset):
-    attrs = {}
-    packet.identity.append(core.ProtocolIdentity(identity, attrs, offset))
+    def __init__(self, packet, offset):
+        super().__init__(packet, offset)
 
+    def get_attributes(self):
+        """Retrieve a set of attributes describing fields in this protocol."""
+        return {}
 
-def prototype(attrstr):
-    pass
+    def set_attributes(self, attrs):
+        """Alter packet data to match a set of protocol attributes."""
+        pass
 
+    @staticmethod
+    def build_attributes(attrstr):
+        """Build attribute dict from string."""
+        attrs = {}
+        return attrs
 
-def modify(packet, ididx, prototype):
-    pass
+    @staticmethod
+    def interpret_packet(packet, offset):
+        """Interpret packet data for this protocol."""
+        instance = ARP(packet, offset)
 
-
-identity = "ip4"
-eth.register(eth.ETHERTYPE_ARP, sys.modules[__name__])
+core.register_protocol(ARP)
+eth.register_ethertype(ARP.name, eth.ETHERTYPE_ARP)
