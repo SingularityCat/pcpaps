6d479891d5037f2a06c688de0779dca65218aa89
Elliot Thomas <e.singularitycat@gmail.com>
2015-03-30 14:06:40 +0100

    Rewrote ip6.py to use new class based interface.

--- src/packet/identity/ip6.py
+++ src/packet/identity/ip6.py
@@ -1,29 +1,33 @@
-import sys
 import struct
 
 from . import core
-from . import ethernet
-
-identifier = "ip6"
+from . import eth
 
 
-def identify(packet, offset):
-    attrs = {}
-    packet.identity.append(core.ProtocolIdentity(identifier, attrs, offset))
+class IPv6(core.Protocol):
+    name = "ip6"
 
+    def __init__(self, packet, offset):
+        super().__init__(packet, offset)
 
-def prototype(attrstr):
-    pass
+    def get_attributes(self):
+        """Retrieve a set of attributes describing fields in this protocol."""
+        return {}
 
+    def set_attributes(self, attrs):
+        """Alter packet data to match a set of protocol attributes."""
+        pass
 
-def modify(packet, ididx, prototype):
-    pass
+    @staticmethod
+    def build_attributes(attrstr):
+        """Build attribute dict from string."""
+        attrs = {}
+        return attrs
 
+    @staticmethod
+    def interpret_packet(packet, offset):
+        """Interpret packet data for this protocol."""
+        instance = IPv6(packet, offset)
 
-def register(ip6proto, offset, module):
-    registry[ip6proto] = module
-
-
-registry = {}
-
-eth.register(eth.ETHERTYPE_IP6, sys.modules[__name__])
+core.register_protocol(IPv6)
+eth.register_ethertype(IPv6.name, eth.ETHERTYPE_IP6)
