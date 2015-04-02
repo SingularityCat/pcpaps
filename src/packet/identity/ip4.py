8c94434220a604dc95d3b624db3300d8f8393b57
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-02 01:49:59 +0000

    Make IPv4 a CarrierProtocol, add stubs. Added IP (v4) protocol registry.

--- src/packet/identity/ip4.py
+++ src/packet/identity/ip4.py
@@ -4,11 +4,26 @@ from . import core
 from . import eth
 
 
-class IPv4(core.Protocol):
+class IPv4(core.CarrierProtocol):
     name = "ip4"
 
+    __slots__ = {"_proto", "_daddr","_saddr"}
+
+
     def __init__(self, packet, offset):
+        """"""
         super().__init__(packet, offset)
+
+
+    def get_route(self):
+        """Returns the route defined by this IP header."""
+        return bytes()
+
+
+    def get_route_reciprocal(self):
+        """Returns the reciprocal route of this IP header."""
+        return bytes()
+
 
     def get_attributes(self):
         """Retrieve a set of attributes describing fields in this protocol."""
@@ -18,16 +33,26 @@ class IPv4(core.Protocol):
         """Alter packet data to match a set of protocol attributes."""
         pass
 
+
     @staticmethod
     def build_attributes(attrstr):
         """Build attribute dict from string."""
         attrs = {}
         return attrs
 
+
     @staticmethod
     def interpret_packet(packet, offset):
         """Interpret packet data for this protocol."""
         instance = IPv4(packet, offset)
 
+
+def register_ip_protocol(protocol, protonum):
+    """Associate a protocl name with a IP protocol number."""
+    registry[protonum] = protocol
+
+registry = {}
+
+# Register protocol, and as an ethertype handler.
 core.register_protocol(IPv4)
 eth.register_ethertype(IPv4.name, eth.ETHERTYPE_IP4)
