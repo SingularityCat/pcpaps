814bde6ebf2074bf757a0dff7f225759e71dccfe
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-04 20:55:46 +0000

    Improved identify pipeline. Packets with an incomplete (deferred) protocol instance will now be buffered until they are complete, or until the source runs dry. The order of the packets remains the same.

--- src/packet/pipeline/identify.py
+++ src/packet/pipeline/identify.py
@@ -1,5 +1,20 @@
 from .. identity import core as identity_core
 def identify(source):
+    # Deferred packets go here.
+    buf = []
+
     for packet in source:
+        # Identify:
         identity_core.root_identify(packet)
-        yield packet
+        if not packet.identity.is_complete():
+            buf.append(packet)
+            continue
+
+        if len(buf) > 0:
+            while len(buf) > 0 and buf[0].identity.is_complete():
+                yield buf.pop(0)
+        else:
+            yield packet
+
+    for leftover in buf:
+        yield buf
