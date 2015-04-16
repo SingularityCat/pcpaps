d08758cbb0dfca087726557e7c877730a3632ef4
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-16 17:47:22 +0100

    Add some ip protocol constants to ip module.

--- src/packet/identity/ip.py
+++ src/packet/identity/ip.py
@@ -6,7 +6,7 @@ Internet Protocol Number registry.
 from . import core
 
 
-def register_ip_protocol(protonum, protoname):
+def register_ip_protocol(protoname, protonum):
     """Add a protocol number <-> name mapping."""
     ip_protocol_registry[protonum] = protoname
 
@@ -18,3 +18,12 @@ def lookup_ip_protocol(protonum):
     return None
 
 ip_protocol_registry = {}
+
+# IANA: http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+PROTO_ICMP = 1
+PROTO_IPV4 = 4
+PROTO_TCP = 6
+PROTO_UDP = 17
+PROTO_IPV6 = 41
+PROTO_IPV6_ICMP = 58
+PROTO_ETHERIP = 97
