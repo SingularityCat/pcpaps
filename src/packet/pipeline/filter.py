80c8f96dcdf5a47afddfba7df99129ee656e9e9e
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-16 01:15:31 +0000

    renamed permit to keep in filter.

--- src/packet/pipeline/filter.py
+++ src/packet/pipeline/filter.py
@@ -1,5 +1,5 @@
-PERMIT=True
-DISCARD=False
+KEEP = True
+DISCARD = False
 
 def identity_match(protocol_instances, prototypes):
     """Compares a packet's identity list (protocol instances) to a list of
@@ -15,23 +15,25 @@ Prototypes are a (name, attrs) tuple."""
     return match
 
 
-# Any identity matching something in the permit set is permitted.
+# Any identity matching something in the keep set is kept.
 # Any identity matching something in the discard set is discarded.
 # Otherwise, the policy boolean is used.
-def filter(source, permit=None, discard=None, policy=PERMIT):
-    if permit is None:
-        permit = set()
+def filter(source, keep=None, discard=None, policy=KEEP):
+    if keep is None:
+        keep = set()
     if discard is None:
         discard = set()
 
     for packet in source:
         yield_packet = policy
-        for prototypes in permit:
+        for prototypes in keep:
             if identity_match(packet.identity, prototypes):
                 yield_packet = True
+                break
         else:
             for prototypes in discard:
                 if identity_match(packet.identity, prototypes):
                     yield_packet = False
+                    break
         if yield_packet:
             yield packet
