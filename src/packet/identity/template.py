e1b693debd4620f7ae1398fa0064b22d3ed1626e
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-16 01:21:06 +0000

    Added template protocol file for convenience.

---
+++ src/packet/identity/template.py
@@ -0,0 +1,63 @@
+from .. import common
+
+from . import core
+
+from .core import uint16pack, uint16unpack
+
+# RFC 826:
+
+
+class Template(core.Protocol):
+    name = "template"
+
+        __slots__ = {}
+
+    def __init__(self, data, prev):
+        super().__init__(data, prev)
+        self._calculate_offsets()
+
+    def _calculate_offsets(self):
+        # Fixed offsets.
+
+
+    def replace_hosts(self, hostmap):
+
+
+    def get_attributes(self):
+        """Retrieve a set of attributes describing fields in this protocol."""
+        return {
+        }
+
+    def set_attributes(self, attrs):
+        """Alter packet data to match a set of protocol attributes."""
+
+
+    # <attr> = <key> "=" <value>
+    # <attrstr> = <attr> | <attr> ";" <attrstr>
+    #
+    # Attributes are seperated by semicolons, which contain colon-seperated key-value pairs.
+    # Valid keys are 
+    @staticmethod
+    def build_attributes(attrstr):
+        """Creates a set of attributes from an attribute string."""
+        attrdict = {}
+
+        attrs = attrstr.split(";")
+        for attr in attrs:
+            try:
+                # Split into kvp.
+                k, v = attr.split("=")
+
+            except ValueError:
+                # Skip malformed attribute.
+                pass
+
+        return attrdict
+
+    @staticmethod
+    def interpret_packet(data, parent):
+        """Interpret packet data for this protocol."""
+        instance = Template(data, parent)
+        return instance
+
+core.register_protocol(Template)
