e65e74cc20d347115119d83e712ef1b2d9ffbf3c
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-23 18:55:35 +0000

    Implement more of the TCP handler.

--- src/packet/identity/tcp.py
+++ src/packet/identity/tcp.py
@@ -5,23 +5,59 @@ from . import ip
 
 from .core import uint16pack, uint16unpack
 
+# RFC 793: Transmission Control Protocol.
+
+#  0                   1                   2                   3
+#  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |          Source Port          |       Destination Port        |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |                        Sequence Number                        |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |                    Acknowledgment Number                      |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |  Data |           |U|A|P|R|S|F|                               |
+# | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+# |       |           |G|K|H|T|N|N|                               |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |           Checksum            |         Urgent Pointer        |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |                    Options                    |    Padding    |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |                             data                              |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+tcp_routes = {}
+
+class TCPStateMachine:
+    __slots__ = {"initiator_route", "responder_route"}
+    def __init__(self, ir, rr):
+        self.initiator_route = ir
+        self.responder_route = rr
+
 
 class TCP(core.CarrierProtocol):
     name = "tcp"
 
-    __slots__ = {}
+    __slots__ = {"_sport", "_dport",
+                 "_seqnum",
+                 "_acknum",
+                 "_offset_flags", "_window"
+                 "_chksum", "_urgptr"}
 
     def __init__(self, data, prev):
         super().__init__(data, prev)
         self._calculate_offsets()
 
     def _calculate_offsets(self):
-        # Fixed offsets.
-        pass
-
-
-    def replace_hosts(self, hostmap):
-        pass
+        self._sport = slice(0, 2)
+        self._dport = slice(2, 4)
+        self._seqnum = slice(4, 8)
+        self._acknum = slice(8, 12)
+        self._offset_flags = slice(12, 14)
+        self._window = slice(14, 16)
+        self._chksum = slice(16, 18)
+        self._urgptr = slice(18, 20)
 
 
     def get_attributes(self):
@@ -31,7 +67,17 @@ class TCP(core.CarrierProtocol):
 
     def set_attributes(self, attrs):
         """Alter packet data to match a set of protocol attributes."""
+        pass
 
+    def recalculate_checksums(self):
+        """Recalculate the checksum for this TCP header."""
+        if self.next is not None:
+            self.next.recalculate_checksums()
+
+    @staticmethod
+    def reset_state():
+        """Resets the tcp_routes dict."""
+        tcp_routes.clear()
 
     # <attr> = <key> "=" <value>
     # <attrstr> = <attr> | <attr> ";" <attrstr>
@@ -59,6 +105,23 @@ class TCP(core.CarrierProtocol):
     def interpret_packet(data, parent):
         """Interpret packet data for this protocol."""
         instance = TCP(data, parent)
+
+        if parent is None or not isinstance(parent, core.CarrierProtocol):
+            # Cannot possibly track without a source/destination!
+            return instance
+
+        # Both route and rev_route should resolve to the same
+        # TCP state machine object.
+        route = parent.get_route()
+
+        if route not in tcp_routes:
+            rev_route = parent.get_route_reciprocal()
+            tsm = TCPStateMachine(route, rev_route)
+
+            tcp_routes[route] = tsm
+            tcp_routes[rev_route] = tsm
+
+
         return instance
 
 core.register_protocol(TCP)
