f6b827a826358fb4d23053ae7312ae828b4105e0
Elliot Thomas <e.singularitycat@gmail.com>
2015-04-23 18:44:20 +0000

    Made generic IP checksum algorithm in ip module.

--- src/packet/identity/ip.py
+++ src/packet/identity/ip.py
@@ -1,9 +1,37 @@
 """
 ip: 
-Internet Protocol Number registry.
+Internet Protocol Number registry and checksum algorithm.
 """
+import struct
 
 from . import core
+
+
+# The checksum algorithm is:
+#      The checksum field is the 16 bit one's complement of the one's
+#      complement sum of all 16 bit words in the header.  For purposes of
+#      computing the checksum, the value of the checksum field is zero.
+
+def checksum(data):
+    """General implementation of the IP checksum algorithm."""
+    n16words, rem = divmod(len(data), 2)
+
+    if rem != 0:
+        # Argument is not in terms of 16-bit words.
+        return None
+
+    # generate list of 16-bit words.
+    words = [word for word in struct.Struct("!"+"H"*n16words).unpack(data)]
+
+    ocsum = 0
+    for word in words:
+        # One's complement addition of 16-bit words.
+        acc = ocsum + word
+        carry = acc >> 16
+        ocsum = (acc + carry) & 0xFFFF
+
+    b1, b2 = divmod(ocsum, 256)
+    return bytes((~b1 & 0xFF, ~b2 & 0xFF))
 
 
 def register_ip_protocol(protoname, protonum):
@@ -12,10 +40,12 @@ def register_ip_protocol(protoname, protonum):
 
 
 def lookup_ip_protocol(protonum):
+    """Looks up a protocol name from number."""
     if protonum in ip_protocol_registry:
         protoname = ip_protocol_registry[protonum]
         return core.lookup_protocol(protoname)
     return None
+
 
 ip_protocol_registry = {}
 
